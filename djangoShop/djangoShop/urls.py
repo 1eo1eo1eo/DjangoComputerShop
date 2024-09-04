6909c7145131076dc93cd30c215c8470797a339c
Lev <lafitel31@gmail.com>
2024-09-04 19:21:34 +1100

    include path to main app

--- djangoShop/djangoShop/urls.py
+++ djangoShop/djangoShop/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
+    path("", include("main.urls")),
     path("admin/", admin.site.urls),
 ]
