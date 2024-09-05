967c496e2fd339f39ab3a0b6a200fc42d704f1a8
Lev <lafitel31@gmail.com>
2024-09-05 15:26:36 +1100

    create class CategoryAdmin and ProductAdmin

--- djangoShop/goods/admin.py
+++ djangoShop/goods/admin.py
@@ -3,5 +3,15 @@ from django.contrib import admin
 from goods.models import Category, Product
 
 
-admin.site.register(Category)
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields: dict[str, tuple] = {"slug": ("name",)}
+    list_display = ("id", "name")
+    list_display_links = ("id", "name")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields: dict[str, tuple] = {"slug": ("name",)}
+    list_display = ("id", "name")
+    list_display_links = ("id", "name")
