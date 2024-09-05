3f36a0d8b19e51e51238874077ad1ce8b12899d0
Lev <lafitel31@gmail.com>
2024-09-05 13:41:26 +1100

    create Category and Product models

--- djangoShop/goods/models.py
+++ djangoShop/goods/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name="Title",
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        blank=True,
+        null=True,
+        verbose_name="URL",
+    )
+
+    class Meta:
+        db_table = "category"
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name="Title",
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        blank=True,
+        null=True,
+        verbose_name="URL",
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Description",
+    )
+    image = models.ImageField(
+        upload_to="goods_images",
+        blank=True,
+        null=True,
+        verbose_name="Imagies",
+    )
+    price = models.DecimalField(
+        default=None,
+        max_digits=7,
+        decimal_places=2,
+        verbose_name="Price",
+    )
+    discount = models.DecimalField(
+        default=None,
+        max_digits=7,
+        decimal_places=2,
+        verbose_name="Discount %",
+    )
+    quantity = models.PositiveIntegerField(
+        default=0,
+        verbose_name="Quantity",
+    )
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.CASCADE,
+        verbose_name="Category",
+    )
+
+    class Meta:
+        db_table = "product"
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
