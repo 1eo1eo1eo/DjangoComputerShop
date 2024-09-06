523ec2478e31a48b5e53f19666ef3be77a27af16
Lev <lafitel31@gmail.com>
2024-09-06 14:22:03 +1100

    add media

--- djangoShop/djangoShop/settings.py
+++ djangoShop/djangoShop/settings.py
@@ -124,7 +124,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_URL = "media/"
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+MEDIA_ROOT = BASE_DIR / "media"
 
 INTERNAL_IPS = [
     "127.0.0.1",
--- djangoShop/djangoShop/urls.py
+++ djangoShop/djangoShop/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 from .settings import DEBUG
+from . import settings
 
 
 urlpatterns = [
@@ -31,3 +33,5 @@ if DEBUG:
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
     ]
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
