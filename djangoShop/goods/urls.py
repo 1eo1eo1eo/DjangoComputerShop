b490fc6062a7d7e37c1eb0b6fa615655f652a606
Lev <lafitel31@gmail.com>
2024-09-07 18:14:51 +1100

    add url dispatching by product_id

--- djangoShop/goods/urls.py
+++ djangoShop/goods/urls.py
@@ -6,5 +6,5 @@ app_name = "catalog"
 
 urlpatterns = [
     path("", views.catalog, name="catalog"),
-    path("product/", views.product, name="product"),
+    path("product/<int:product_id>/", views.product, name="product"),
 ]
