9798158463073538e8c490fa6fd3566629e1bea0
Lev <lafitel31@gmail.com>
2024-09-10 15:59:23 +1000

    show category by slug

--- djangoShop/goods/urls.py
+++ djangoShop/goods/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "catalog"
 
 urlpatterns = [
-    path("", views.catalog, name="catalog"),
+    path("<slug:category_slug>/", views.catalog, name="catalog"),
     path("product/<slug:product_slug>/", views.product, name="product"),
 ]
