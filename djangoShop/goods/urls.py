410388bac53666faad33891ff2316c6f651d1757
Lev <lafitel31@gmail.com>
2024-09-10 19:39:08 +1000

    add page pagination

--- djangoShop/goods/urls.py
+++ djangoShop/goods/urls.py
@@ -6,5 +6,6 @@ app_name = "catalog"
 
 urlpatterns = [
     path("<slug:category_slug>/", views.catalog, name="catalog"),
+    path("<slug:category_slug>/<int:page>", views.catalog, name="catalog"),
     path("product/<slug:product_slug>/", views.product, name="product"),
 ]
--- djangoShop/goods/views.py
+++ djangoShop/goods/views.py
@@ -1,17 +1,23 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
+
 from .models import Product
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
 
     if category_slug == "all-products":
         queryset = Product.objects.order_by("id")
     else:
         queryset = get_list_or_404(Product.objects.filter(category__slug=category_slug))
 
+    paginator = Paginator(queryset, 3)
+    current_page = paginator.page(page)
+
     context: dict = {
         "title": "BYD - Catalog",
-        "goods": queryset,
+        "goods": current_page,
+        "slug_url": category_slug,
     }
     return render(request, "goods/catalog.html", context)
 
