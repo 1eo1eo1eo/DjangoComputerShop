58229ad71e069aa3305ffad53e6d28d25921aafe
Lev <lafitel31@gmail.com>
2024-09-12 14:44:29 +1000

    create bash script(auto migrations and loaddata to db)

---
+++ afterreload.sh
@@ -0,0 +1,11 @@
+docker compose -f docker-compose-dev.yaml up -d --build
+
+cd djangoShop
+
+poetry run python3 manage.py migrate
+
+poetry run python3 manage.py loaddata fixtures/goods/category.json
+
+poetry run python3 manage.py loaddata fixtures/goods/product.json
+
+poetry run python3 manage.py createsuperuser
