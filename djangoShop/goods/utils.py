553007da2a8fc0f6a82db8af3948e5b850e303a0
Lev <lafitel31@gmail.com>
2024-09-12 19:02:13 +1000

    create find by words with length grater than 2

--- djangoShop/goods/utils.py
+++ djangoShop/goods/utils.py
@@ -7,3 +7,12 @@ def query_search(query):
 
     if query.isdigit() and len(query) <= 5:
         return Product.objects.filter(id=int(query))
+
+    keywords = [word for word in query.split() if len(word) > 2]
+
+    q_objects = Q()
+
+    for token in keywords:
+        q_objects |= Q(name__icontains=token)
+
+    return Product.objects.filter(q_objects)
