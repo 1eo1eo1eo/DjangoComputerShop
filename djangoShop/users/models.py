c1e24f43eb1256559f12f0bd82cf692bc88a21ff
Lev <lafitel31@gmail.com>
2024-09-14 13:55:07 +1000

    create User model

--- djangoShop/users/models.py
+++ djangoShop/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField(
+        upload_to="users_images",
+        blank=True,
+        null=True,
+        verbose_name="Avatar",
+    )
+
+    class Meta:
+        db_table: str = "Users"
+        verbose_name: str = "User"
+        verbose_name_plural: str = "Users"
+
+    def __str__(self) -> str:
+        return self.username
