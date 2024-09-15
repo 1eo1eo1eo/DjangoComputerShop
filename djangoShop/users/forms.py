212e5e1de69809fa7e7baae15d1491e8c72b8d46
Lev <lafitel31@gmail.com>
2024-09-15 18:02:45 +1000

    create UserRegistrationForm

--- djangoShop/users/forms.py
+++ djangoShop/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
@@ -9,3 +9,21 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ["username", "password"]
+
+
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        ]
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.CharField()
