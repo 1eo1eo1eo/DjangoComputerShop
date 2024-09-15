bf8fa09aac1627a57eb4dd0904bffb20dd28f7fc
Lev <lafitel31@gmail.com>
2024-09-15 15:52:45 +1000

    create auth validation

--- djangoShop/users/views.py
+++ djangoShop/users/views.py
@@ -1,14 +1,33 @@
 from typing import TYPE_CHECKING
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib import auth
+
+from users.forms import UserLoginForm
 
 if TYPE_CHECKING:
     from django.http import HttpResponse, HttpRequest
 
 
 def login(request: "HttpRequest") -> "HttpResponse":
+
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("main:home"))
+    else:
+        form = UserLoginForm()
+
     context: dict = {
         "title": "BYD - Authorization",
+        "form": form,
     }
 
     return render(request, "users/login.html", context)
