1e3c8058bb730b617765de290a80a6f574f6d119
Lev <lafitel31@gmail.com>
2024-09-17 15:23:49 +1000

    add url to users/basket.html

--- djangoShop/users/urls.py
+++ djangoShop/users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("login/", views.login, name="login"),
     path("registration/", views.registration, name="registration"),
     path("profile/", views.profile, name="profile"),
+    path("basket/", views.basket, name="basket"),
     path("logout/", views.logout, name="logout"),
 ]
