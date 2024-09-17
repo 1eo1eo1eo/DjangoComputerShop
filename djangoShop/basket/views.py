cfe8e53b41a8dbf422509332e14b8168353a370c
Lev <lafitel31@gmail.com>
2024-09-17 15:19:35 +1000

    create basket views

--- djangoShop/basket/views.py
+++ djangoShop/basket/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def basket_add(
+    request,
+    product_id,
+):
+    pass
+
+
+def basket_change(
+    request,
+    product_id,
+):
+    pass
+
+
+def basket_remove(
+    request,
+    product_id,
+):
+    pass
