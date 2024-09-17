e77c278a78872bc6a1c4173c5d8f60aff7ca126d
Lev <lafitel31@gmail.com>
2024-09-17 15:17:26 +1000

    create local basket urls

--- djangoShop/basket/urls.py
+++ djangoShop/basket/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+
 app_name = "basket"
 
 urlpatterns = [
-    path("", views., name=""),
+    path("basket-add/<int:product_id>/", views.basket_add, name="basket_add"),
+    path("basket-change/<int:product_id>/", views.basket_change, name="basket_change"),
+    path("basket-remove/<int:product_id>/", views.basket_remove, name="basket_remove"),
 ]
