71c0bb82ae0be470b8eccddbafc07c2c209969e1
Lev <lafitel31@gmail.com>
2024-09-18 21:51:51 +1000

    add basket_add function filling

--- djangoShop/basket/views.py
+++ djangoShop/basket/views.py
@@ -1,22 +1,39 @@
-from django.shortcuts import render
+from math import prod
+from django.shortcuts import redirect, render
+
+from basket.models import Basket
+from goods.models import Product
 
 
 def basket_add(
     request,
-    product_id,
+    product_slug,
 ):
-    pass
+    product = Product.objects.get(slug=product_slug)
+
+    if request.user.is_authenticated:
+        baskets = Basket.objects.filter(user=request.user, product=product)
+
+        if baskets.exists():
+            basket = baskets.first()
+            if basket:
+                basket.quantity += 1
+                basket.save()
+        else:
+            Basket.objects.create(user=request.user, product=product, quantity=1)
+
+    return redirect(request.META["HTTP_REFERER"])
 
 
 def basket_change(
     request,
-    product_id,
+    product_slug,
 ):
     pass
 
 
 def basket_remove(
     request,
-    product_id,
+    product_slug,
 ):
     pass
