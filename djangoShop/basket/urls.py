0a62bf4ae3c3a3778bdb1a4ccf7113ed4746fdd0
Lev <lafitel31@gmail.com>
2024-09-18 21:51:30 +1000

    some small refactoring

--- djangoShop/basket/urls.py
+++ djangoShop/basket/urls.py
@@ -1,12 +1,24 @@
 from django.urls import path
 
-from . import views
+from basket import views
 
 
 app_name = "basket"
 
 urlpatterns = [
-    path("basket-add/<int:product_id>/", views.basket_add, name="basket_add"),
-    path("basket-change/<int:product_id>/", views.basket_change, name="basket_change"),
-    path("basket-remove/<int:product_id>/", views.basket_remove, name="basket_remove"),
+    path(
+        "basket_add/<slug:product_slug>/",
+        views.basket_add,
+        name="basket_add",
+    ),
+    path(
+        "basket_change/<slug:product_slug>/",
+        views.basket_change,
+        name="basket_change",
+    ),
+    path(
+        "basket_remove/<slug:product_slug>/",
+        views.basket_remove,
+        name="basket_remove",
+    ),
 ]
