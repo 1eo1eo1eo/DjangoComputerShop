13ddc5d87cd689c4ed7f96591aa015371d726d97
Lev <lafitel31@gmail.com>
2024-09-22 17:18:21 +1000

    add session creation for unauth users

--- djangoShop/basket/utils.py
+++ djangoShop/basket/utils.py
@@ -4,3 +4,10 @@ from basket.models import Basket
 def get_user_baskets(request):
     if request.user.is_authenticated:
         return Basket.objects.filter(user=request.user)
+
+    if not request.session.session_key:
+        request.session.create()
+
+    return Basket.objects.filter(
+        session_key=request.session.session_key,
+    )
