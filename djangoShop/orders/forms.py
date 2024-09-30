8156052e93c45e8e3518da1faa2530c98b5afdd9
Lev <lafitel31@gmail.com>
2024-09-30 19:26:18 +1000

    add choices to ChoiceFields

--- djangoShop/orders/forms.py
+++ djangoShop/orders/forms.py
@@ -2,9 +2,15 @@ from django import forms
 
 
 class CreateOrderForm(forms.Form):
+
+    choices = [
+        ("0", "False"),
+        ("1", "True"),
+    ]
+
     first_name = forms.CharField()
     last_name = forms.CharField()
     phone_number = forms.CharField()
-    requires_delivery = forms.ChoiceField()
+    requires_delivery = forms.ChoiceField(choices=choices)
     delivery_address = forms.CharField(required=False)
-    payment_on_get = forms.ChoiceField()
+    payment_on_get = forms.ChoiceField(choices=choices)
