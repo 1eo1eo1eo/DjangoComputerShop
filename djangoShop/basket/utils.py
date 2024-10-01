097b1cea26dccf181ae62856927c5087ca8ab283
Lev <lafitel31@gmail.com>
2024-10-01 20:04:45 +1000

    add select_related by product

--- djangoShop/basket/utils.py
+++ djangoShop/basket/utils.py
@@ -3,11 +3,13 @@ from basket.models import Basket
 
 def get_user_baskets(request):
     if request.user.is_authenticated:
-        return Basket.objects.filter(user=request.user)
+        return Basket.objects.filter(
+            user=request.user,
+        ).select_related("product")
 
     if not request.session.session_key:
         request.session.create()
 
     return Basket.objects.filter(
         session_key=request.session.session_key,
-    )
+    ).select_related("product")
