367130ffd8b277c2820bbd79d51add5be10af2f2
Lev <lafitel31@gmail.com>
2024-10-01 18:20:23 +1000

    customized models in admin panel and create tabular models

--- djangoShop/orders/admin.py
+++ djangoShop/orders/admin.py
@@ -3,11 +3,89 @@ from django.contrib import admin
 from orders.models import Order, OrderItem
 
 
+class OrderTabularAdmin(admin.TabularInline):
+    model = Order
+    fields = [
+        "requires_delivery",
+        "status",
+        "payment_on_get",
+        "is_paid",
+        "created_timestamp",
+    ]
+
+    search_fields = [
+        "requires_delivery",
+        "payment_on_get",
+        "is_paid",
+        "created_timestamp",
+    ]
+    readonly_fields = [
+        "created_timestamp",
+    ]
+    extra = 0
+
+
+class OrderItemTabularAdmin(admin.TabularInline):
+    model = OrderItem
+    fields = [
+        "product",
+        "name",
+        "price",
+        "quantity",
+    ]
+    search_fields = [
+        "product",
+        "name",
+    ]
+    extra = 0
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display_links = [
+        "id",
+        "user",
+    ]
+    list_display = [
+        "id",
+        "user",
+        "requires_delivery",
+        "status",
+        "payment_on_get",
+        "is_paid",
+        "created_timestamp",
+    ]
+
+    search_fields = [
+        "id",
+    ]
+
+    readonly_fields = [
+        "created_timestamp",
+    ]
+    list_filter = [
+        "requires_delivery",
+        "status",
+        "payment_on_get",
+        "is_paid",
+        "created_timestamp",
+    ]
+    inlines = [
+        OrderItemTabularAdmin,
+    ]
 
 
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        "order",
+        "product",
+        "name",
+        "price",
+        "quantity",
+    ]
+    search_fields = [
+        "order",
+        "product",
+        "name",
+    ]
