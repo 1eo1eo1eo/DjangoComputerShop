78426ea5ce806daa7c41544a7316a6816a7ef4e3
Lev <lafitel31@gmail.com>
2024-10-01 19:30:30 +1000

    create custom phone number validator

--- djangoShop/orders/forms.py
+++ djangoShop/orders/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 
 
@@ -14,3 +15,15 @@ class CreateOrderForm(forms.Form):
     requires_delivery = forms.ChoiceField(choices=choices)
     delivery_address = forms.CharField(required=False)
     payment_on_get = forms.ChoiceField(choices=choices)
+
+    def clean_phone_number(self):
+        data = self.cleaned_data["phone_number"]
+
+        if not data.isdigit():
+            raise forms.ValidationError("Phone number should have only numbers")
+
+        pattern = re.compile(r"^\d{10}$")
+        if not pattern.match(data):
+            raise forms.ValidationError("Phone number wrong format")
+
+        return data
