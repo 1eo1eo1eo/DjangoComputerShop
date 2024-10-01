9e6ae8515302f4b3b1bf3483b8581d73566d3678
Lev <lafitel31@gmail.com>
2024-10-01 20:05:04 +1000

    create orders showing in user profile

--- djangoShop/users/views.py
+++ djangoShop/users/views.py
@@ -5,8 +5,10 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.contrib import auth, messages
+from django.db.models import Prefetch
 
 from basket.models import Basket
+from orders.models import Order, OrderItem
 from users.forms import UserLoginForm, UserProfileForm, UserRegistrationForm
 
 if TYPE_CHECKING:
@@ -99,9 +101,21 @@ def profile(request: "HttpRequest") -> "HttpResponse":
     else:
         form = UserProfileForm(instance=request.user)
 
+    orders = (
+        Order.objects.filter(user=request.user)
+        .prefetch_related(
+            Prefetch(
+                "orderitem_set",
+                queryset=OrderItem.objects.select_related("product"),
+            )
+        )
+        .order_by("-id")
+    )
+
     context: dict = {
         "title": "BYD - Profile",
         "form": form,
+        "orders": orders,
     }
 
     return render(request, "users/profile.html", context)
