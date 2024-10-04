bbdb1072d27f5867780356593bcc8cef1f78b9f7
Lev <lafitel31@gmail.com>
2024-10-04 15:52:04 +1000

    change urls to CBV

--- djangoShop/goods/urls.py
+++ djangoShop/goods/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "catalog"
 
 urlpatterns = [
-    path("search/", views.catalog, name="search"),
-    path("<slug:category_slug>/", views.catalog, name="catalog"),
+    path("search/", views.CatalogView.as_view(), name="search"),
+    path("<slug:category_slug>/", views.CatalogView.as_view(), name="catalog"),
     path("product/<slug:product_slug>/", views.ProductView.as_view(), name="product"),
 ]
