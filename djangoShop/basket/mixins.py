26abff29571eae1c17883c0c8b1f52274f9d7cf2
Lev <lafitel31@gmail.com>
2024-10-06 17:46:37 +1000

    Create basket mixin

---
+++ djangoShop/basket/mixins.py
@@ -0,0 +1,34 @@
+from django.template.loader import render_to_string
+from django.urls import reverse
+
+from basket.models import Basket
+from basket.utils import get_user_baskets
+
+
+class BasketMixin:
+    def get_basket(self, request, product=None):
+        if request.user.is_authenticated:
+            query_kwargs = {"user": request.user}
+        else:
+            query_kwargs = {"session_key": request.session.session_key}
+
+        if product:
+            query_kwargs["product"] = product
+
+        return Basket.objects.filter(**query_kwargs).first()
+
+    def render_basket(self, request):
+        user_baskets = get_user_baskets(request)
+        context = {
+            "baskets": user_baskets,
+        }
+
+        referer = request.META.get("HTTP_REFERER")
+        if reverse("orders:create_order") in referer:
+            context["order"] = True
+
+        return render_to_string(
+            "includes/included_basket.html",
+            context,
+            request=request,
+        )
