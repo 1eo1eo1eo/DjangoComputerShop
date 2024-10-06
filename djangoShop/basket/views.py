2078ed9225969816d225b7f6d7b2e6080ff74e03
Lev <lafitel31@gmail.com>
2024-10-06 17:58:57 +1000

    change basket_change to BasketChangeView(class based view)

--- djangoShop/basket/views.py
+++ djangoShop/basket/views.py
@@ -41,92 +41,23 @@ class BasketAddView(BasketMixin, View):
         return JsonResponse(response_data)
 
 
-# def basket_add(
-#     request,
-# ):
+class BasketChangeView(BasketMixin, View):
 
-#     product_id = request.POST.get("product_id")
-#     product = Product.objects.get(id=product_id)
+    def post(self, request):
+        basket_id = request.POST.get("cart_id")
+        quantity = request.POST.get("quantity")
+        basket = self.get_basket(request, basket_id=basket_id)
 
-#     if request.user.is_authenticated:
-#         baskets = Basket.objects.filter(user=request.user, product=product)
+        basket.quantity = quantity
+        basket.save()
 
-#         if baskets.exists():
-#             basket = baskets.first()
-#             if basket:
-#                 basket.quantity += 1
-#                 basket.save()
-#         else:
-#             Basket.objects.create(user=request.user, product=product, quantity=1)
+        response_data = {
+            "message": "Quantity changed",
+            "cart_items_html": self.render_basket(request),
+            "quantity": quantity,
+        }
 
-#     else:
-#         baskets = Basket.objects.filter(
-#             session_key=request.session.session_key,
-#             product=product,
-#         )
-
-#         if baskets.exists():
-#             basket = baskets.first()
-#             if basket:
-#                 basket.quantity += 1
-#                 basket.save()
-#         else:
-#             Basket.objects.create(
-#                 session_key=request.session.session_key,
-#                 product=product,
-#                 quantity=1,
-#             )
-
-#     user_baskets = get_user_baskets(request)
-#     basket_items_html = render_to_string(
-#         "includes/included_basket.html",
-#         {"baskets": user_baskets},
-#         request=request,
-#     )
-
-#     response_data = {
-#         "message": "Товар добавлен в корзину",
-#         "cart_items_html": basket_items_html,
-#     }
-
-#     return JsonResponse(response_data)
-
-
-def basket_change(
-    request,
-):
-
-    basket_id = request.POST.get("cart_id")
-    quantity = request.POST.get("quantity")
-
-    basket = Basket.objects.get(id=basket_id)
-
-    basket.quantity = quantity
-    basket.save()
-
-    user_baskets = get_user_baskets(request)
-
-    context = {
-        "baskets": user_baskets,
-    }
-
-    referer = request.META.get("HTTP_REFERER")
-    if reverse("orders:create_order") in referer:
-        context["order"] = True
-
-    cart_items_html = render_to_string(
-        "includes/included_basket.html",
-        context,
-        request=request,
-    )
-
-    response_data = {
-        "message": "Quantity changed",
-        "cart_items_html": cart_items_html,
-        "quantity": quantity,
-    }
-
-    return JsonResponse(response_data)
+        return JsonResponse(response_data)
 
 
 def basket_remove(
