d26a364ceed173ced2bf612d8a93585095891870
Lev <leo144112@yandex.ru>
2024-10-19 18:32:27 +1000

    add RabbitBase, RabbitException and UsersUpdatesRabbit to __all__

---
+++ djangoShop/rabbitmq/common/__init__.py
@@ -0,0 +1,9 @@
+__all__ = (
+    "RabbitBase",
+    "RabbitException",
+    "UsersUpdatesRabbit",
+)
+
+from .base import RabbitBase
+from .exc import RabbitException
+from .user_updates_rabbit import UsersUpdatesRabbit
