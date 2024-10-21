8974537982877aefde45d6a2f84c761edf30ee79
Lev <leo144112@yandex.ru>
2024-10-21 19:05:28 +1000

    create tests for logout view

---
+++ djangoShop/tests/users/test_logout_view.py
@@ -0,0 +1,15 @@
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_logout(client, create_test_user):
+    client.login(
+        username="testuser",
+        password="testpassword",
+    )
+    url = reverse("users:logout")
+    response = client.get(url)
+
+    assert response.status_code == 302
+    assert response.url == reverse("main:home")
