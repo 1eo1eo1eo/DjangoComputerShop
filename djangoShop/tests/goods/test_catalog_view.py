2f608639294080b7bb51c0a79900f481e8658ca3
Lev <leo144112@yandex.ru>
2024-10-22 13:31:34 +1000

    create catalog views tests

---
+++ djangoShop/tests/goods/test_catalog_view.py
@@ -0,0 +1,81 @@
+import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_pagination_is_correct(client, setUpTestData):
+    category, category_all = setUpTestData
+
+    response = client.get(
+        reverse(
+            "catalog:catalog",
+            kwargs={"category_slug": category.slug},
+        )
+    )
+
+    assert response.status_code == 200
+    assert "goods" in response.context
+    assert len(response.context["goods"]) == 3
+
+    response = client.get(
+        reverse(
+            "catalog:catalog",
+            kwargs={"category_slug": category.slug},
+        )
+        + "?page=2"
+    )
+
+    assert response.status_code == 200
+    assert len(response.context["goods"]) == 3
+
+
+@pytest.mark.django_db
+def test_search_functionality(client, setUpTestData):
+    search_query = "Product"
+    response = client.get(
+        reverse("catalog:search") + f"?q={search_query}",
+    )
+
+    assert response.status_code == 200
+
+    assert "goods" in response.context
+    products = response.context["goods"]
+
+    assert len(products) == 3
+    assert products[0].name == "Product 4"
+
+    response = client.get(
+        reverse("catalog:search") + f"?q={search_query}&page=2",
+    )
+
+    assert len(response.context["goods"]) == 2
+
+
+@pytest.mark.django_db
+def test_on_sale_filter(client, setUpTestData):
+    category, category_all = setUpTestData
+
+    response = client.get(
+        reverse(
+            "catalog:catalog",
+            kwargs={"category_slug": "gpus"},
+        )
+        + "?on_sale=on"
+        + "&order_by=default"
+    )
+
+    assert response.status_code == 200
+    assert len(response.context["goods"]) == 3
+
+    response = client.get(
+        reverse(
+            "catalog:catalog",
+            kwargs={"category_slug": "gpus"},
+        )
+        + "?on_sale=on"
+        + "&order_by=default"
+        + "&page=2"
+    )
+
+    assert response.status_code == 200
+    assert len(response.context["goods"]) == 1
