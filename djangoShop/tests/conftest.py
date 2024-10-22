127d9684ce298d5b15f313ece7b3b91f1e3af251
Lev <leo144112@yandex.ru>
2024-10-22 13:29:44 +1000

    add setUpTestData to conftest

--- djangoShop/tests/conftest.py
+++ djangoShop/tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from goods.models import Product, Category
 
 
 @pytest.fixture
@@ -7,3 +8,38 @@ def create_test_user(db, django_user_model):
         username="testuser",
         password="testpassword",
     )
+
+
+### Goods
+
+
+@pytest.fixture
+def setUpTestData(db):
+    category = Category.objects.create(
+        name="GPUs",
+        slug="gpus",
+    )
+    category_all = Category.objects.create(
+        name="All products",
+        slug="all-products",
+    )
+
+    for i in range(5):
+        Product.objects.create(
+            name=f"Product {i}",
+            slug=f"product-{i}",
+            category=category,
+            price=100 + i,
+            discount=i if i % 2 == 0 else 0,
+        )
+
+    for i in range(5):
+        Product.objects.create(
+            name=f"Good {i}",
+            slug=f"good-{i}",
+            category=category,
+            price=100 + i,
+            discount=i if i % 2 == 0 else 0,
+        )
+
+    return category, category_all
